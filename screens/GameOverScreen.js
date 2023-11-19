dab12162bb18a8065271ca81b25d4fe149547e01
frodriguezbeistegui <frodriguezbeistegui@gmail.com>
2023-11-18 23:27:47 -0300

    better styling and game over layout

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,6 +1,6 @@
 import { View, StyleSheet, Image, Text } from "react-native"
 import Colors from "../constants/colors"
-import { Title } from "../components"
+import { PrimaryButton, Title } from "../components"
 
 
 function GameOverScreen () {
@@ -12,7 +12,11 @@ function GameOverScreen () {
             <View style={styles.imageContainer}>
                 <Image source={require(`../assets/images/success.png`)} style={styles.image} />
             </View>
-            <Text>Your pohone needed X rounds to guess the number Y.</Text>
+            <Text style={ styles.summaryText }>
+                Your pohone needed <Text style={styles.highlight}>X</Text> rounds to 
+                guess the number <Text style={styles.highlight}>Y</Text>.
+            </Text>
+            <PrimaryButton>Start New Game</PrimaryButton>
         </View>
     )
 };
@@ -37,6 +41,16 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: '100%',
+    },
+    summaryText: {
+        fontFamily: 'open-sans',
+        fontSize: 24,
+        textAlign: 'center',
+        marginBottom: 24,
+    },
+    highlight: {
+        fontFamily: 'open-sans-bold',
+        color: Colors.primary500
     }
 })
 
